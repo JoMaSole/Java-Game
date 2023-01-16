99ab563074605a483803387e1d14217268189afc
JoanSole <joan.m.sole@hotmail.com>
2023-01-16 17:49:33 -0300

    mapa

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
 	
@@ -15,15 +16,16 @@ public class GamePanel extends JPanel implements Runnable{
 	final int originalTileSize = 16; //16x16
 	final int scale = 3;
 	
-	public final int titleSize = originalTileSize * scale;
-	final int maxScreenCol = 16;
-	final int maxScreenRow = 12;
-	final int screenWidth = titleSize * maxScreenCol; //768
-	final int screenHeight = titleSize * maxScreenRow; //576
+	public final int tileSize = originalTileSize * scale;
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol; //768
+	public final int screenHeight = tileSize * maxScreenRow; //576
 	
 	//FPS
 	int FPS = 60;
 	
+	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread; //cuando inicia llama automaticamente al metodo run
 	Player player = new Player(this,keyH);
@@ -113,12 +115,14 @@ public class GamePanel extends JPanel implements Runnable{
 		
 	}
 	//con este metodo dibujamos el pj
+	
 	public void paintComponent(Graphics g) { 
 		//es el lapiz o pincel
 		super.paintComponent(g);
 		
 		Graphics2D g2 = (Graphics2D)g;
 		
+		tileM.draw(g2); //primero se dibuja el fondo y despues el pj
 		player.draw(g2);
 		g2.dispose();
 		
