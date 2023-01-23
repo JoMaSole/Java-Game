2b73107e9b88ff03c1a633468b4d651a01d5244e
JoanSole <joan.m.sole@hotmail.com>
2023-01-23 17:29:03 -0300

    final mapa

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -110,7 +110,17 @@ public class TileManager {
 			int screenX = worldX - gp.player.worldX + gp.player.screenX;
 			int screenY = worldY - gp.player.worldY + gp.player.screenY;
 			
-			g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+			//con este if hacemos que dibuje las baldosas de al rededor del jugador, 
+			//para que no relantice el juego dibujando el mapa entero todo el tiempo
+			
+			if(worldX + gp.tileSize> gp.player.worldX - gp.player.screenX && 
+			   worldX - gp.tileSize< gp.player.worldX + gp.player.screenX &&
+			   worldY + gp.tileSize> gp.player.worldY - gp.player.screenY &&
+			   worldY - gp.tileSize< gp.player.worldY + gp.player.screenY){
+
+				   g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+
+			   }
 			worldCol++;
 			
 			if(worldCol == gp.maxWorldCol) {
