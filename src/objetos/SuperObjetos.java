7c6ce5f0920996c53c3f3d90a1a693b7180ecc4e
JoanSole <joan.m.sole@hotmail.com>
2023-01-27 12:38:23 -0300

    7

---
+++ src/objetos/SuperObjetos.java
@@ -0,0 +1,12 @@
+package objetos;
+
+import java.nio.Buffer;
+
+public class SuperObjetos {
+
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int  worldX, worldY;
+    
+}
